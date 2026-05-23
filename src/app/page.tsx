d568682f2792b63a9624558a43a57476c19066bf
TIS0N <adam.tisoncik@gmail.com>
2026-05-23 14:12:44 +0200

    next push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ export default function HomePage() {
       <h1 className="text-4xl font-bold">Cyber Awareness App</h1>
 
       <p className="mt-4 text-gray-600">Learn how to stay safe online.</p>
-      <p className="mt-2 text-gray-600">
-        Hello and welcome to my cyber awareness app!
-      </p>
     </main>
   );
 }
