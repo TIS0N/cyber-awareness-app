cb5020a30765123a657a60d03d899ff7e4c5b25c
TIS0N <adam.tisoncik@gmail.com>
2026-07-05 15:37:49 +0200

    Cleanup and small fixes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   devIndicators: false,
+  allowedDevOrigins: ["192.168.0.90"],
 };
-
-module.exports = {
-  allowedDevOrigins: ['192.168.0.90'],
-}
 
 export default nextConfig;
